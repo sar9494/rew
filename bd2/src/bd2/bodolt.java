06df79a4b7af77afec1f465939c3c0a01bcaefb2
lenovo <lenovo@DESKTOP-A36SGJ2>
2024-03-26 18:42:34 +0800

    bd2

---
+++ bd2/src/bd2/bodolt.java
@@ -0,0 +1,51 @@
+package bd2;
+
+public class bodolt {
+	
+	int ajiludur1=6;
+	int ajilUdur2=5;
+	int hicheeltsag1_3=4;
+	int hicheelTsag4_8=34;
+	int hicheelTsag9_10=6;
+	int dundMin=45;   //dund sur 1tsag=45min 1978
+	int deedTsag=3;
+	int deedMin=90;  //deed sur 1tsag=90min 
+	int subTime1_5=4;
+	int subTime6_12=6;
+	
+public int jil(int f, int l) {
+	boolean old;
+	boolean young;
+	if((f>=1978&&l<=1993)) {
+		old=true;
+	}
+	else if((f>=2006&&l<=2022)) {
+		young=true;
+	}
+	else {
+		old=young=false;
+	}
+	
+	return 0;
+}
+public void weekday() {
+	//hicheellej baigaa 7 honogiin toog bodoh
+}
+public void hTsag(){
+	//hicheellej bgaa tsagiig tootsoh
+}
+public void hHonog() {
+	//hicheeliin udruud
+}
+public void angi() {
+	//oniig ashiglan heddugeer angi gedegiig tootsooloh
+}
+public void dadlagaTsag() {
+	//oyutanii dadlagiin hugatsaag bodoh
+}
+public void enjoyTime() {
+	//sonirhson hicheel useh tsag
+}
+
+
+}
