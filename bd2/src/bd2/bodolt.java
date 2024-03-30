a3f3abdfe41594816bcc1bfbc86ce3b7e5ae021d
sar9494 <158149533+sar9494@users.noreply.github.com>
2024-03-30 22:01:48 +0800

    last sunday bodolt.java

--- bd2/src/bd2/bodolt.java
+++ bd2/src/bd2/bodolt.java
@@ -6,10 +6,11 @@ public class bodolt {
 	int hicheelTsag4_8=34;
 	int hicheelTsag9_10=36;
 	int dundMin=45;   //dund sur 1tsag=45min 1978
-	int deedTsag=3;
+	int deedTsag=18;
 	int deedMin=90;  //deed sur 1tsag=90min 
 	int subTime1_5=20;
 	int subTime6_12=30;
+	String fday="fri";
 	
 public int jil(int f, int l) {
 	boolean old;
@@ -23,8 +24,40 @@ public int jil(int f, int l) {
 	
 	return 0;
 }
-public void weekday() {
+public int lSunday(int a) {
+	int jil=a-1978, b=jil;
+	int k = 0,l=270;
+	int on=1978;
+		while(jil>=0) {
+			int m=(on-1976)%4;
+			if(m==3) {
+				l+=366;
+				k=(l-3)%7;		
+				}
+			else if(jil==b){
+				k=l%7;
+				l+=3;
+			}
+			else {
+				l+=365;
+				k=(l-3)%7;
+			}
+			jil-=1;
+			on+=1;
+		}
+	return 31-k;
+}
+
+public int fMonday(int a) {
+	int jil=a-1978, b=jil;
+	int k = 0,l=0;
+	int on=1978;
+		
+	return 1+k;
+}
+public void weekday(int a, int b) {
 	//hicheellej baigaa 7 honogiin toog bodoh
+	
 }
 public void hTsag(){
 	//hicheellej bgaa tsagiig tootsoh
@@ -41,6 +74,16 @@ public void dadlagaTsag() {
 public void enjoyTime() {
 	//sonirhson hicheel useh tsag
 }
+public static void main(String arg[]) {
+	bodolt a=new bodolt();
+	int on=1981;
+	int l=a.lSunday(on);
+	int k=a.fMonday(on);
+	System.out.println(on+1+" on 5 sar suuliin sunday "+l);
+	System.out.println(on+" onii 9 sariin ehnii monday "+k);
+}
+}
+
 
 
 }
